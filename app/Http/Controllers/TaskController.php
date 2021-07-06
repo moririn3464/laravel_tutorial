df796290fa97bf51786ff7ceeb0753cb0de8811f
moririn3464 <moririn3464@gmail.com>
2021-07-06 17:19:10 +0900

    タスク一覧表示作成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,17 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\Folder;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
+
+      //全てのフォルダ取得
       $folders = Folder::all();
+
+      //選んでいるフォルダのIDを取得
+      $current_folder = Folder::find($id);
+
+      //選んでいるフォルダのタスクを全て取得する
+      $tasks = $current_folder->tasks()->get();
 
       return view('tasks/index',[
         'folders' => $folders,
-        'current_folder_id' => $id,
+        'current_folder_id' => $current_folder->id,
+        'tasks' => $tasks,
       ]);
 
     }
