0e439f74ce961af04d2b1037d0d06b4a62dd3fe9
moririn3464 <moririn3464@gmail.com>
2021-07-13 15:03:41 +0900

    認証周り

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,23 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\FolderController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::group(['middleware' => 'auth'], function() {
+  Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
-Route::post('/folders/create', [FolderController::class, 'create']);
+  Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
 
-Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
-Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
+  Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
+  Route::post('/folders/create', [FolderController::class, 'create']);
 
-Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
+  Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
+  Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
+
+  Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
+  Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
+
+});
+
+Auth::routes();
+
